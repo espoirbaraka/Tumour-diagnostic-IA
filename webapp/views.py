48590aa60de1cb93e625590f54f85c0a310c8a20
Baraka Bigega Espoir <49196039+espoirbaraka@users.noreply.github.com>
2024-06-08 22:55:34 +0200

    Template bootstrap

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
+def BASE(request):
+    template = loader.get_template('base.html')
+    return HttpResponse(template.render())
 # Create your views here.
