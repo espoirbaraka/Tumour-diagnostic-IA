781b094a87ef292cc0623dd508f41e77fb05e195
Baraka Bigega Espoir <49196039+espoirbaraka@users.noreply.github.com>
2024-06-09 08:34:02 +0200

    Diagnostic

--- diagnostic/views.py
+++ diagnostic/views.py
@@ -2,14 +2,18 @@ from django.shortcuts import render
 from pgmpy.models import BayesianNetwork
 from pgmpy.factors.discrete import TabularCPD
 import networkx as nx
+from django.http import HttpResponseBadRequest
 from pgmpy.sampling import BayesianModelSampling
 from pgmpy.inference import VariableElimination
 import pylab as plt
 
 def tumour_result(request):
     if request.method=='POST':
-        coma=request.POST.get('coma')
-        headache=request.POST.get('headache')
+        try:
+            coma=int(request.POST.get('coma'))
+            headache=int(request.POST.get('headache'))
+        except (ValueError, TypeError):
+            return HttpResponseBadRequest("Invalid input. Please enter valid integers for coma and headache.")
         medical_model = BayesianNetwork([
         #cause       effet
         ("Metastatic","Serum"),
@@ -68,7 +72,33 @@ def tumour_result(request):
             cpd_coma,
             cpd_headache
         )
+
+        inference = VariableElimination(medical_model)
+        answer = inference.query(
+        variables=['Coma'],
+        evidence={
+            'Tumour': coma,
+            'Serum': headache
+        })
+
+        if answer is not None:
+            # result_list = []
+            # for state, prob in zip(answer.state_names['Tumour'], answer.values):
+            #         result_list.append(f"Tumour({state}): {prob:.4f}")
+            max_prob_index = answer.values.argmax()  # Récupérer l'indice de la probabilité maximale
+            max_state = answer.state_names['Coma'][max_prob_index]  # Récupérer l'état correspondant
+            max_prob = answer.values[max_prob_index]
+            if(max_state==0):
+                res=f"Tu n'as pas la tumeur. Tu as une une probabilité de {max_prob}"
+            elif(max_state==1):
+                res=f"Tu as une probalibilte d'avoir la tumeur. Tu as une une probabilité de {max_prob}"
+        else:
+             print("Erreur de retour")
+
+        context = {
+            'res': res
+        }
         # Create your views here.
 
         template = 'tumour_result.html'
-        return render(request, template)
+        return render(request, template, context)
