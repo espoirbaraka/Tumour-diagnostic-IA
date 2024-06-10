838c4c62febd707c19dfc932551704b09ba55825
Baraka Bigega Espoir <49196039+espoirbaraka@users.noreply.github.com>
2024-06-10 07:28:22 +0200

    Finalizing

--- diagnostic/views.py
+++ diagnostic/views.py
@@ -277,7 +277,7 @@ def calcium_result(request):
             'Headache': headache,
             'Tumour': tumour,
             'Coma': coma,
-            'Tumour': tumour
+            'Metastatic': cancer
         })
 
         if answer is not None:
@@ -297,4 +297,101 @@ def calcium_result(request):
         }
 
         template = 'calcium_result.html'
+        return render(request, template, context)
+
+def headache_result(request):
+    if request.method=='POST':
+        try:
+            coma=int(request.POST.get('coma'))
+            calcium=int(request.POST.get('calcium'))
+            tumour=int(request.POST.get('tumour'))
+            cancer=int(request.POST.get('cancer'))
+        except (ValueError, TypeError):
+            return HttpResponseBadRequest("Invalid input. Please enter valid integers for coma and headache.")
+        medical_model = BayesianNetwork([
+        #cause       effet
+        ("Metastatic","Serum"),
+        ("Metastatic","Tumour"),
+        ("Serum","Coma"),
+        ("Tumour","Coma"),
+        ("Tumour","Headache"),
+        ])
+
+        cpd_metastic = TabularCPD(
+            variable="Metastatic",
+            variable_card=2, #Cardinalite 2 car nous avons juste 0 et 1 (Vrai ou faux)
+            #       False(0)    True(1)
+            values=[[0.8],[0.2]]
+        )
+
+        cpd_serum = TabularCPD(
+            variable="Serum",
+            variable_card=2,
+            #       1-P(Serum)                   P(Serum)                   
+            values=[[0.8, 0.2], [0.2, 0.8]],
+            evidence=["Metastatic"],
+            evidence_card=[2]
+        )
+
+        cpd_tumour = TabularCPD(
+            variable="Tumour",
+            variable_card=2,
+            #       1-P(Tumour)                   P(Tumour)                   
+            values=[[0.95, 0.8], [0.05, 0.2]],
+            evidence=["Metastatic"],
+            evidence_card=[2]
+        )
+        cpd_coma = TabularCPD(
+            variable="Coma",
+            variable_card=2,
+            #       1-P(Coma)                   P(Coma)                   
+            values=[[0.95, 0.2, 0.2, 0.2], [0.05, 0.8, 0.8, 0.8]],
+            evidence=["Serum","Tumour"],
+            evidence_card=[2,2]
+        )
+
+        cpd_headache = TabularCPD(
+            variable="Headache",
+            variable_card=2,
+            #       1-P(Tumour)                   P(Tumour)                   
+            values=[[0.4, 0.2], [0.6, 0.8]],
+            evidence=["Tumour"],
+            evidence_card=[2]
+        )
+
+        medical_model.add_cpds(
+            cpd_metastic,
+            cpd_serum,
+            cpd_tumour,
+            cpd_coma,
+            cpd_headache
+        )
+
+        inference = VariableElimination(medical_model)
+        answer = inference.query(
+        variables=['Headache'],
+        evidence={
+            'Serum': calcium,
+            'Tumour': tumour,
+            'Coma': coma,
+            'Metastatic': cancer
+        })
+
+        if answer is not None:
+            max_prob_index = answer.values.argmax()  # Récupérer l'indice de la probabilité maximale
+            max_state = answer.state_names['Headache'][max_prob_index]  # Récupérer l'état correspondant
+            max_prob = answer.values[max_prob_index]
+            if(max_state==0):
+                res=f"Tu as une faible probabilité d'avoir le maut de tete. La probabilité de ne pas l'avoir est de {max_prob*100} %"
+            elif(max_state==1):
+                res=f"Tu as une probalibilte d'avoir le maut de tete. La probabilité de l'avoir est de {max_prob*100} %"
+        else:
+             print("Erreur de retour")
+
+        context = {
+            'res': res,
+            'etat':max_state
+        }
+
+        template = 'headache_result.html'
         return render(request, template, context)
